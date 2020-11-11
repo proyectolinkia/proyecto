663c43cf44ced3950d888c1d62f73f8e8f818800
ifthencode <jacobomonterogil.profesional@gmail.com>
2020-11-11 09:32:37 +0100

    verificacion login preparada

--- Proyectos/ProyectoDAM/app/src/main/java/com/example/proyectodam/ActivityMain.java
+++ Proyectos/ProyectoDAM/app/src/main/java/com/example/proyectodam/ActivityMain.java
@@ -10,10 +10,18 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 
 public class ActivityMain extends AppCompatActivity {
@@ -22,6 +30,7 @@ public class ActivityMain extends AppCompatActivity {
     Connection con;
     EditText usu, psw;
     ProgressBar progressBar;
+	String valor;
     //final String urlWebService= "http://35.205.20.239/sql.php?sentenciasql=Select%20nombreEmpresa%20FROM%20Empresas%20where%20nombreEmpresa=%27demoEmpresa%27";
 
     @Override
@@ -46,14 +55,14 @@ public class ActivityMain extends AppCompatActivity {
         Intent intent = new Intent(this, ActivityPerfilComerciante.class);
         usu = (EditText) findViewById(R.id.campoUsuario); //ojo
         psw = (EditText) findViewById(R.id.campoPsw);// OJO
-
-        downloadJSON("http://35.205.20.239/sql.php?sentenciasql=Select%20pswrd%20FROM%20Empresas%20where%20nombreEmpresa=%27"+usu+"%27");
-
-        startActivity(intent);
+       String nombreEmpresa=usu.getText().toString();
+      //  downloadJSON("http://35.205.20.239/sql.php?sentenciasql=Select%20pswrd%20FROM%20Empresas%20where%20nombreEmpresa=%27"+usu+"%27");
+     downloadJSON("http://35.205.20.239/sql.php?sentenciasql=Select%20pswrd%20FROM%20Empresas%20where%20nombreEmpresa=%27"+nombreEmpresa+"%27",intent);
+     //   startActivity(intent);
 
     }
 
-    private void downloadJSON(final String urlWebService) {
+    private void downloadJSON(final String urlWebService,Intent intent) {
 
         class DownloadJSON extends AsyncTask<Void, Void, String> {
 
@@ -65,13 +74,46 @@ public class ActivityMain extends AppCompatActivity {
 
             @Override
             protected void onPostExecute(String s) {
+                JSONArray jsonArray = null;
+                String sha1;
                 super.onPostExecute(s);
                 Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
-                 //  try {
-                //  loadIntoListView(s);
-                //  } catch (JSONException e) {
-                //      e.printStackTrace();
-                //   }
+                try {
+                    jsonArray = new JSONArray(s);
+                    JSONObject obj = jsonArray.getJSONObject(0);
+                    valor = obj.getString("pswrd");
+                } catch (JSONException e) {
+                    // e.printStackTrace();
+                }
+
+                //   String sha1 = jsonArray.;
+                //  String sha2 = "";
+
+                // With the java libraries
+
+                MessageDigest digest = null;
+                try {
+                    digest = MessageDigest.getInstance("SHA-1");
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+                digest.reset();
+                try {
+                    digest.update(psw.getText().toString().getBytes("utf8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
+
+                if (sha1.equals(valor)) {
+                    startActivity(intent);
+                } else {
+                    Toast toast1 =
+                            Toast.makeText(getApplicationContext(),
+                                    "Los datos no son corrrectos", Toast.LENGTH_SHORT);
+
+                    toast1.show();
+                }
             }
 
             @Override
@@ -81,11 +123,16 @@ public class ActivityMain extends AppCompatActivity {
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
                     StringBuilder sb = new StringBuilder();
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    String json;
+                    String obtenido = null;
+                    String json = " ";
                     while ((json = bufferedReader.readLine()) != null) {
                         sb.append(json + "\n");
                     }
-                    return sb.toString().trim();
+                    if((json == null)&&sb.toString().equals("[]\n")) {
+                        obtenido = "[{\"pswrd\":\"11111\"}]";
+
+                    }else{obtenido=sb.toString().trim();};
+                    return obtenido;
                 } catch (Exception e) {
                     return null;
                 }
@@ -94,5 +141,4 @@ public class ActivityMain extends AppCompatActivity {
         DownloadJSON getJSON = new DownloadJSON();
         getJSON.execute();
     }
-
 }
